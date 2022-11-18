42372c4b0dee236e3cec6cbce382dc71ae6bd25c
filipchukdv <filipchukdv@gmail.com>
2022-11-18 12:01:54 +0200

    Адаптивна верста + адаптивна графіка

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,16 @@
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-    };
+      
+  };
+   const body = document.querySelector('.js-body');
+
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+      body.classList.toggle('modal-open');
     }
   })();
